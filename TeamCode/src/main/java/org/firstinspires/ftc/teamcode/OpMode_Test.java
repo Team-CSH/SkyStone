fdde5d72df37fcd820692b4cc7d3b4ad47da9dbb
Luca Cristea <lucacristea04@gmail.com>
2020-08-29 00:43:16 +0300

    Stil researching...write the complete code soon

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Test.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class OpMode_Test extends OpMode {
+    //creez un enumerator(group of constants pt states);
+
+    /*
+        @param enum - special class that represent a group of constants
+     */
+    private enum States {
+        INIT_STATE,
+        LIFT_UP_STATE,
+        LIFT_DOWN_STATE,
+        SUCTION_INTAKE_STATE,
+        SUCTION_OUTTAKE_STATE,
+        ARM_EXTENDED_STATE,
+        ARM_RETRACTED_STATE,
+        PLIERS_ON_STATE,
+        PLIERS_OFF_STATE,
+        STOP_STATE,
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+
+
+
+
+}
